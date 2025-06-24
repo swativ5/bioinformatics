e3eb827605a51711d5633dfbbb42b5552ff3e106
swativ5 <swativ.05.v@gmail.com>
2025-06-24 09:45:03 +0530

    Week 2

--- Genome Sequencing/02_MaximalNonBranchingPaths.py
+++ Genome Sequencing/02_MaximalNonBranchingPaths.py
@@ -34,6 +34,8 @@ MaximalNonBranchingPaths(Graph)
 
 def MaximalNonBranchingPaths(Graph):
     Paths = []
+    nodes1in1out = set()
+    nExplored = set()
 
     all_nodes = set(Graph.keys()).union(*Graph.values())
     indegree = {node: 0 for node in all_nodes}
@@ -49,28 +51,44 @@ def MaximalNonBranchingPaths(Graph):
             if outdegree[node_v] > 0:
                 for node_w in Graph[node_v]:
                     NonBranchingPath = [node_v, node_w]
-                    while (indegree[node_w] == outdegree[node_w]):
-                        node_u = Graph[node_w][0]  # Get the unique outgoing node
+                    while (indegree[node_w] == 1 and outdegree[node_w] == 1):
+                        nExplored.add(node_w)
+                        node_u = Graph[node_w][0]
                         NonBranchingPath.append(node_u)
                         node_w = node_u
                     Paths.append(NonBranchingPath)
+        else:
+            nodes1in1out.add(node_v)
 
-    for node in Graph:
-        if indegree[node] == 1 and outdegree[node] == 1:
-            cycle = []
-            current = node
-
-            while current in Graph and Graph[current]:
-                cycle.append(current)
-                next_node = Graph[current][0]
-
-                if next_node == node:
-                    cycle.append(next_node)
+    for node_v in nodes1in1out:
+        if node_v not in nExplored:
+            node_w = node_v
+            nbPath = []
+            while node_w in nodes1in1out:
+                nbPath.append(node_w)
+                if node_w == node_v and len(nbPath) > 1:
+                    Paths.append(nbPath)
+                    for node in nbPath:
+                        nExplored.add(node)
                     break
-                current = next_node
+                node_w = Graph[node_w][0]
 
-            if cycle not in Paths:
-                Paths.append(cycle)
+    # for node in Graph:
+    #     if indegree[node] == 1 and outdegree[node] == 1:
+    #         cycle = []
+    #         current = node
+
+    #         while current in Graph and Graph[current]:
+    #             cycle.append(current)
+    #             next_node = Graph[current][0]
+
+    #             if next_node == node:
+    #                 cycle.append(next_node)
+    #                 break
+    #             current = next_node
+
+    #         if cycle not in Paths:
+    #             Paths.append(cycle)
 
     s = ""
     for line in Paths:
@@ -104,9 +122,9 @@ if __name__ == "__main__":
             print(f"Test {i + 1} is Failed")
             print(file_output_text, "\n", f_output)
 
-    # file_input = open(f"/home/swativ5/Downloads/dataset_30188_16.txt")
-    # file_input_text = file_input.read()
-    # k, d, Patterns = parsing(file_input_text)
-    # f_output = StringReconstructionReadPairs(k, d, Patterns)
-    # f = open("test.txt", "w")
-    # f.write(f_output)
+    file_input = open(f"/home/swativ5/Downloads/dataset_30188_16.txt")
+    file_input_text = file_input.read()
+    Graph = parsing(file_input_text)
+    f_output = MaximalNonBranchingPaths(Graph)
+    f = open("test.txt", "w")
+    f.write(f_output)
